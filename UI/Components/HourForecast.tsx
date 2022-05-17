4601b4991f0ca891c2110643537142df92c31f3b
yefimchuk <dmitriy.yefimchuk@gmail.com>
2022-05-17 17:21:34 +0300

    was styled hourly forecast

--- UI/Components/HourForecast.tsx
+++ UI/Components/HourForecast.tsx
@@ -1,44 +1,61 @@
 import React from "react";
-import {Image, ScrollView, StyleSheet, Text, View} from "react-native";
-import {useSelector} from "react-redux";
-import {fetchHourlyWeatherSelector} from "../../BLL/Weather/weather.selector";
+import { Image, ScrollView, StyleSheet, Text, View } from "react-native";
+import { useSelector } from "react-redux";
+import {
+  fetchHourlyWeatherSelector,
+  fetchingSelector,
+} from "../../BLL/Weather/weather.selector";
 import moment from "moment";
 
 const HourForecast = () => {
   const hourForecastData = useSelector(fetchHourlyWeatherSelector);
-  console.log(hourForecastData)
+  console.log(hourForecastData);
   /*
         console.log(moment(1652788800 ).format('HH'))
     */
+  const isFetching = useSelector(fetchingSelector);
 
   return (
+    !isFetching && (
       <View style={styles.hourForecast}>
         <ScrollView horizontal={true} style={styles.scrollView}>
           {hourForecastData
-              ? hourForecastData
-                  .filter((item: object, idx: number) => idx < 24)
-                  .map((key: { id: number, dt: number, temp: number, weather: [icon: any] }, inx: number) => (
-
-                      <View style={styles.weather} key={inx}>
-                        <Text>
-                          {moment(key.dt * 1000).format("HH") ===
-                          moment(+Date.now()).format("HH")
-                              ? "now"
-                              : moment(key.dt * 1000).format("HH")}
-                        </Text>
-                        <Image
-                            style={styles.currentForecast__img}
-                            source={{
-                              uri: `http://openweathermap.org/img/wn/${key.weather[0].icon}@2x.png`,
-                            }}
-                            resizeMode={"contain"}
-                        />
-                        <Text>{Math.round(key.temp) + "°"}</Text>
-                      </View>
-                  ))
-              : null}
+            ? hourForecastData
+                .filter((item: object, idx: number) => idx < 24)
+                .map(
+                  (
+                    key: {
+                      id: number;
+                      dt: number;
+                      temp: number;
+                      weather: [icon: any];
+                    },
+                    inx: number
+                  ) => (
+                    <View style={styles.weather} key={inx}>
+                      <Text style={styles.hourForecast__text}>
+                        {moment(key.dt * 1000).format("HH") ===
+                        moment(+Date.now()).format("HH")
+                          ? "now"
+                          : moment(key.dt * 1000).format("HH")}
+                      </Text>
+                      <Image
+                        style={styles.currentForecast__img}
+                        source={{
+                          uri: `http://openweathermap.org/img/wn/${key.weather[0].icon}@2x.png`,
+                        }}
+                        resizeMode={"contain"}
+                      />
+                      <Text style={styles.hourForecast__text}>
+                        {Math.round(key.temp) + "°"}
+                      </Text>
+                    </View>
+                  )
+                )
+            : null}
         </ScrollView>
       </View>
+    )
   );
 };
 
@@ -46,12 +63,17 @@ const styles = StyleSheet.create({
   scrollView: {
     flexDirection: "row",
   },
+  hourForecast__text: {
+    fontSize: 18,
+    fontWeight: "400",
+    color: "#ffffff",
+  },
   currentForecast__img: {
     width: 40,
     height: 40,
-
   },
   weather: {
+    marginLeft: 10,
     justifyContent: "center",
     alignItems: "center",
   },
@@ -59,7 +81,9 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "flex-end",
     flex: 0.14,
-    borderWidth: 1,
+    borderTopWidth: 1,
+    borderBottomWidth: 1,
+    borderColor: "rgba(220,220,220,0.71)",
   },
 });
 export default HourForecast;
