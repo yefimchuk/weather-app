98dae0e982c7a0e2f0dc6fcc278362be4cad6be5
yefimchuk <dmitriy.yefimchuk@gmail.com>
2022-05-22 15:45:01 +0300

    fetch full info about weather

--- UI/Components/CurrentForecast.tsx
+++ UI/Components/CurrentForecast.tsx
@@ -1,6 +1,6 @@
-import React, {useRef} from "react";
-import {Animated, Image, ScrollView, StyleSheet, Text, View} from "react-native";
-import {useSelector} from "react-redux";
+import React, { useRef } from "react";
+import { Animated, Image, StyleSheet, Text, View } from "react-native";
+import { useSelector } from "react-redux";
 import {
   fetchCurrentWeatherSelector,
   fetchingSelector,
@@ -9,8 +9,9 @@ import {
 import ForecastSearch from "./ForecastSearch";
 import HourForecast from "./HourForecast";
 import DailyForecast from "./DailyForecast";
+import OtherInfo from "./OtherInfo";
 
-const CurrentForecast = ({scrolling}: any) => {
+const CurrentForecast = ({ scrolling }: any) => {
   const currentWeatherData = useSelector(fetchCurrentWeatherSelector);
   const weatherData = useSelector(fetchWeatherDataSelector);
   const isFetching = useSelector(fetchingSelector);
@@ -37,127 +38,107 @@ const CurrentForecast = ({scrolling}: any) => {
     extrapolate: "clamp",
   });
   return (
-      <View>
-        <Animated.View style={{
-          height: 120, justifyContent: "center", transform: [{
-            translateY: scaleSearch
-          }]
+    <View>
+      <Animated.View
+        style={{
+          height: 120,
+          justifyContent: "center",
+          transform: [
+            {
+              translateY: scaleSearch,
+            },
+          ],
+        }}
+      >
+        <ForecastSearch />
+      </Animated.View>
 
-        }}>
-          <ForecastSearch/>
-        </Animated.View>
+      <View style={styles.currentForecast}>
+        {!isFetching && (
+          <View>
+            {currentWeatherData.weather && (
+              <View style={styles.currentForecast__FlexCol}>
+                <Animated.View
+                  style={{
+                    height: 110,
+                    transform: [
+                      {
+                        translateY: scaleSearch,
+                      },
+                    ],
+                  }}
+                >
+                  <Text style={styles.currentForecast__city}>
+                    {weatherData.name}
+                  </Text>
+                  <View style={styles.currentForecast__FlexRow}>
+                    <Image
+                      style={styles.currentForecast__img}
+                      source={{
+                        uri: `http://openweathermap.org/img/wn/${currentWeatherData.weather[0].icon}@2x.png`,
+                      }}
+                      resizeMode={"contain"}
+                    />
+                    <Text style={styles.currentForecast__description}>
+                      {currentWeatherData.weather[0].main}
+                    </Text>
+                  </View>
+                </Animated.View>
 
-        <View style={styles.currentForecast}>
-          {!isFetching && (
-              <View>
-                {currentWeatherData.weather && (
-                    <View style={styles.currentForecast__FlexCol}>
-                      <Animated.View style={{
-                        height: 110, transform: [{
-                          translateY: scaleSearch
-                        }]
-                      }}>
-                        <Text style={styles.currentForecast__city}>
-                          {weatherData.name}
-                        </Text>
-                        <View style={styles.currentForecast__FlexRow}>
-                          <Image
-                              style={styles.currentForecast__img}
-                              source={{
-                                uri: `http://openweathermap.org/img/wn/${currentWeatherData.weather[0].icon}@2x.png`,
-                              }}
-                              resizeMode={"contain"}
-                          />
-                          <Text style={styles.currentForecast__description}>
-                            {currentWeatherData.weather[0].main}
-                          </Text>
-                        </View>
-                      </Animated.View>
-
-
-                      <Animated.ScrollView
-                          style={{
-                            width: "100%",
-                            transform: [{
-                              translateY: scaleScrollView
-                            }]
-                          }}
-                          onScroll={Animated.event(
-                              [{nativeEvent: {contentOffset: {y: scrollY}}}],
-                              {useNativeDriver: false}
-                          )}
-                      >
-                        <Animated.View
-                            style={{
-                              flexDirection: "column",
-                              justifyContent: "center",
-                              alignItems: "center",
-                              opacity: opacityTemp,
-                            }}
-                        >
-                          <Text style={styles.currentForecast__degrees}>
-                            {" " + Math.round(currentWeatherData.temp) + "°"}
-                          </Text>
-                          <View style={{flexDirection: "row"}}>
-                            <Text
-                                style={styles.currentForecast__hl}
-                            >{`H: ${Math.round(weatherData.main.temp_max)}°`}</Text>
-                            <Text
-                                style={styles.currentForecast__hl}
-                            >{`L: ${Math.floor(weatherData.main.temp_min)}°`}</Text>
-                          </View>
-                        </Animated.View>
-
-                        <Animated.View style={{ position: "relative",top: opacityHourlyForecast}}>
-
-                          <HourForecast/>
-
-                        </Animated.View>
-
-                          <DailyForecast/>
-                          <Animated.View>
-                            <Text>adad</Text>
-                            <Text>adad</Text>
-                            <Text>adad</Text>
-                            <Text>adad</Text>
-                            <Text>adad</Text>
-                            <Text>adad</Text>
-                            <Text>adad</Text>
-                            <Text>adad</Text>
-                            <Text>adad</Text>
-                            <Text>adad</Text>
-                            <Text>adad</Text>
-                            <Text>adad</Text>
-                            <Text>adad</Text>
-                            <Text>adad</Text>
-                            <Text>adad</Text>
-                            <Text>adad</Text>
-                            <Text>adad</Text>
-                            <Text>adad</Text>
-                            <Text>adad</Text>
-                            <Text>adad</Text>
-                            <Text>adad</Text>
-                            <Text>adad</Text>
-                            <Text>adad</Text>
-                            <Text>adad</Text>
-                            <Text>adad</Text>
-                            <Text>adad</Text>
-                            <Text>adad</Text>
-                            <Text>adad</Text>
-                            <Text>adad</Text>
-                            <Text>adad</Text>
-                            <Text>adad</Text>
-                            <Text>adad</Text>
-                          </Animated.View>
-
-
-                      </Animated.ScrollView>
+                <Animated.ScrollView
+                  style={{
+                    width: "100%",
+                    borderWidth: 1,
+                    height: 200,
+                    transform: [
+                      {
+                        translateY: scaleScrollView,
+                      },
+                    ],
+                  }}
+                  onScroll={Animated.event(
+                    [{ nativeEvent: { contentOffset: { y: scrollY } } }],
+                    { useNativeDriver: false }
+                  )}
+                >
+                  <Animated.View
+                    style={{
+                      flexDirection: "column",
+                      justifyContent: "center",
+                      alignItems: "center",
+                      opacity: opacityTemp,
+                    }}
+                  >
+                    <Text style={styles.currentForecast__degrees}>
+                      {" " + Math.round(currentWeatherData.temp) + "°"}
+                    </Text>
+                    <View style={{ flexDirection: "row" }}>
+                      <Text
+                        style={styles.currentForecast__hl}
+                      >{`H: ${Math.round(weatherData.main.temp_max)}°`}</Text>
+                      <Text
+                        style={styles.currentForecast__hl}
+                      >{`L: ${Math.floor(weatherData.main.temp_min)}°`}</Text>
                     </View>
-                )}
+                  </Animated.View>
+
+                  <Animated.View
+                    style={{ position: "relative", top: opacityHourlyForecast }}
+                  >
+                    <HourForecast />
+                  </Animated.View>
+
+                  <DailyForecast />
+                  <Animated.View>
+                    <OtherInfo />
+                  </Animated.View>
+                </Animated.ScrollView>
               </View>
-          )}
-        </View>
+            )}
+          </View>
+        )}
       </View>
+    </View>
   );
 };
 export default CurrentForecast;
@@ -173,7 +154,6 @@ const styles = StyleSheet.create({
   currentForecast: {
     justifyContent: "flex-start",
     alignItems: "center",
-
   },
   currentForecast__FlexRow: {
     flexDirection: "row",
--- UI/Components/OtherInfo.tsx
+++ UI/Components/OtherInfo.tsx
@@ -2,28 +2,96 @@ import React from "react";
 import {Text, View} from "react-native";
 import {useSelector} from "react-redux";
 import {fetchCurrentWeatherSelector} from "../../BLL/Weather/weather.selector";
+import moment from "moment";
 
 const OtherInfo = () => {
-    const currentWeatherData = useSelector(fetchCurrentWeatherSelector);
-    console.log(currentWeatherData);
-    return (
+  const {sunset, sunrise, wind_speed, feels_like, humidity, visibility} = useSelector(fetchCurrentWeatherSelector);
+console.log(humidity)
+  const sunSet = moment(sunset * 1000, true).format("k:kk");
+  const sunRise = moment(sunrise * 1000).format("hh:mm ");
+  const windSpeed = wind_speed + "km/hr";
+  const windFeelsLike = feels_like + " ms";
+  const visibilityWeather = visibility/ 1000 + "km"
+  const humidityWeather = humidity + "%"
+  return (<View
+          style={{
+            height: 480,
+            borderWidth: 1,
+          }}
+      >
         <View>
-<ItemInfo info={currentWeatherData.sunset}/>
+          <ItemInfo
+              titleLeft={"SUNRISE"}
+              titleRight={"SUNSET"}
+              valueLeft={sunRise}
+              valueRight={sunSet}
+          />
         </View>
-    );
+        <View>
+          <ItemInfo
+              titleLeft={"WIND SPEED"}
+              titleRight={"FEELS LIKE"}
+              valueLeft={windSpeed}
+              valueRight={windFeelsLike}
+          />
+        </View>
+        <View>
+          <ItemInfo
+              titleLeft={"VISIBILITY"}
+              titleRight={"HUMIDITY"}
+              valueLeft={visibilityWeather}
+              valueRight={humidityWeather}
+          />
+        </View>
+        <View>
+          <ItemInfo
+              titleLeft={"SUNRISE"}
+              titleRight={"SUNSET"}
+              valueLeft={"5:51"}
+              valueRight={"5:51"}
+          />
+        </View>
+        <View>
+          <ItemInfo
+              titleLeft={"SUNRISE"}
+              titleRight={"SUNSET"}
+              valueLeft={"5:51"}
+              valueRight={"5:51"}
+          />
+        </View>
+      </View>
+  );
 };
 export default OtherInfo;
 
-export const ItemInfo = ({info}: { info: string | number }) => {
-    return (
-        <View
-            style={{
-                borderTopWidth: 1,
-                borderBottomWidth: 1,
-                borderColor: "rgba(220,220,220,0.71)",
-            }}
-        >
-            <Text>adad</Text>
+export const ItemInfo = ({
+                           titleLeft,
+                           titleRight,
+                           valueLeft,
+                           valueRight,
+                         }: any) => {
+  debugger;
+
+  return (
+      <View
+          style={{
+            justifyContent: "space-between",
+            margin: 20,
+            alignItems: "center",
+            flexDirection: "row",
+            borderTopWidth: 1,
+            borderBottomWidth: 1,
+            borderColor: "rgba(220,220,220,0.71)",
+          }}
+      >
+        <View>
+          <Text style={{fontSize: 15}}>{titleLeft}</Text>
+          <Text>{valueLeft}</Text>
         </View>
-    );
+        <View>
+          <Text style={{fontSize: 15}}>{titleRight}</Text>
+          <Text>{valueRight}</Text>
+        </View>
+      </View>
+  );
 };
